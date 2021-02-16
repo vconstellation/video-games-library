ae113cf2268bb597241c1c8707cb02357b478956
vconstellation <64845689+vconstellation@users.noreply.github.com>
2021-02-16 17:09:40 +0100

    Rework front page of the game's DetailView

--- gamelist/models.py
+++ gamelist/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.functions import Coalesce
+from django.db.models.functions import Coalesce, Round
 from PIL import Image
 
 # Create your models here.
